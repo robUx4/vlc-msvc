85a1d459dc52be98c722426c312c22a09caac94a
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-01-12 17:28:10 +0100

    setup-env.bat: Fix for msys2 64bits users

--- scripts/setup-env.bat
+++ scripts/setup-env.bat
@@ -5,6 +5,11 @@ set Path=%Path%;C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE
 set LIB=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\lib\store\arm;C:\Program Files (x86)\Windows Phone Kits\8.1\lib\ARM;%LIB%
 
 :Start
-start /Wait C:\msys32\usr\bin\sh -c "scripts/main.sh %*"
-pause
+
+IF EXIST C:\msys32\usr\bin\sh (
+	set SH_EXEC=C:\msys32\usr\bin\sh
+) ELSE (
+	set SH_EXEC=C:\msys64\usr\bin\sh
+)
+
 exit
