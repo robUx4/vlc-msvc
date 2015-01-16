89a3572218256ae607d256197248000878d7e5a0
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-01-16 15:32:35 +0100

    build-tools: Add an ugly work around
    It seems we can't compress to /dev/null, which is detected as a terminal, making the detection test fail

--- scripts/build-tools.sh
+++ scripts/build-tools.sh
@@ -7,4 +7,7 @@ ROOT_FOLDER=$SCRIPTPATH/..
 
 cd $ROOT_FOLDER/vlc/extras/tools/
 ./bootstrap || exit 1
+# Work around failing tar xz detection on msys2
+touch $ROOT_FOLDER/vlc/extras/tools/.tar
+touch $ROOT_FOLDER/vlc/extras/tools/.xz
 make || exit 1
