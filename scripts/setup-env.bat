417f47e79188166e6834526e53040e2872cca5e4
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-02-05 16:56:19 +0100

    setup-env: Fix WindowsRT environment

--- scripts/setup-env.bat
+++ scripts/setup-env.bat
@@ -1,10 +1,9 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_arm
 
-if "%1" NEQ "WindowsPhone" goto Start
 set Path=%Path%;C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE
-set LIB=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\lib\store\arm;C:\Program Files (x86)\Windows Phone Kits\8.1\lib\ARM;%LIB%
 
-:Start
+IF /I "%1" == "WindowsPhone" set LIB=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\lib\store\arm;C:\Program Files (x86)\Windows Phone Kits\8.1\lib\ARM;%LIB%
+IF /I "%1" == "Windows" set LIB=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\lib\store\arm;%LIB%
 
 IF EXIST C:\msys32\usr\bin\sh.exe (
 	set SH_EXEC=C:\msys32\usr\bin\sh
