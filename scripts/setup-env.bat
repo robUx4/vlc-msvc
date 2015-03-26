7e3b7cb5594f822838fb9f97869348ce82f818ce
Steve Lhomme <robux4@videolabs.io>
2015-03-26 16:15:33 +0100

    use the same msys2 shell that was used to call setup-env.bat
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- scripts/setup-env.bat
+++ scripts/setup-env.bat
@@ -5,10 +5,5 @@ set Path=%Path%;C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE
 IF /I "%1" == "WindowsPhone" set LIB=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\lib\store\arm;C:\Program Files (x86)\Windows Phone Kits\8.1\lib\ARM;%LIB%
 IF /I "%1" == "Windows" set LIB=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\lib\store\arm;%LIB%
 
-IF EXIST C:\msys32\usr\bin\sh.exe (
-	set SH_EXEC=C:\msys32\usr\bin\sh
-) ELSE (
-	set SH_EXEC=C:\msys64\usr\bin\sh
-)
 
-start /Wait %SH_EXEC% -c "scripts/main.sh %* pause"
+%WD%%MSYSCON% --hold always /usr/bin/bash scripts/main.sh %*
