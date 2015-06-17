5d786038d2bfef4572c486cdcb8242664726e583
Steve Lhomme <robUx4@gmail.com>
2015-06-17 16:03:29 +0200

    try to use VS 2015 to build for arm

--- scripts/setup-env.bat
+++ scripts/setup-env.bat
@@ -1,16 +1,21 @@
-rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_arm
-rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_arm store
+@IF EXIST "%VS140COMNTOOLS%vsvars32.bat" goto vs2015
+@IF EXIST "%VS120COMNTOOLS%vsvars32.bat" goto vs2013
 
-IF /I "%1" == "WindowsPhone" set VCVAR=x86_arm
-IF /I "%1" == "Windows"      set VCVAR=x86
+:vs2015
+@call "%VS140COMNTOOLS%vsvars32.bat"
+@goto setupenv
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %VCVAR%
-rem store forces %_app.dll runtime that isn't available #wtf
+:vs2013
+@call "%VS120COMNTOOLS%vsvars32.bat"
+@goto setupenv
 
-rem set Path=%Path%;C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE;C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE
+:setupenv
+@IF /I "%1" == "WindowsPhone" set VCVAR=x86_arm
+@IF /I "%1" == "Windows"      set VCVAR=x86
+@rem adding "store" forces %_app.dll runtime that aren't available
 
-rem IF /I "%1" == "WindowsPhone" set LIB=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\store\arm;C:\Program Files (x86)\Windows Phone Kits\8.1\lib\ARM;%LIB%
-rem IF /I "%1" == "Windows" set LIB=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\store\arm;%LIB%
+call "%VSINSTALLDIR%VC\vcvarsall.bat" %VCVAR%
 
+IF /I "%1" == "WindowsPhone" call "%VCINSTALLDIR%\bin\vcvarsphoneall.bat" %VCVAR%
 
 %WD%%MSYSCON% --hold always /usr/bin/bash scripts/main.sh %*
