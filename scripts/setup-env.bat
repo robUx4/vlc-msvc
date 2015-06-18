3a4b19fe4bad4df0e83ce45d9a381689f5b03711
Steve Lhomme <robux4@videolabs.io>
2015-06-18 16:02:12 +0200

    link with the right store libs when needed VCVarsPhoneQueryRegistry should look for WindowsPhoneApp\v8.1

--- scripts/setup-env.bat
+++ scripts/setup-env.bat
@@ -5,12 +5,10 @@
 
 :vs2015
 @set VSVARS="%VS140COMNTOOLS%vsvars32.bat"
-@set STORE_VARIANT=store
 @goto setupenv
 
 :vs2013
 @set VSVARS="%VS120COMNTOOLS%vsvars32.bat"
-@set STORE_VARIANT=
 @goto setupenv
 
 :setupenv
@@ -20,17 +18,19 @@
 
 
 :setup_WindowsPhone
-call %VSVARS% %STORE_VARIANT%
-call "%VSINSTALLDIR%VC\vcvarsall.bat" x86_arm %STORE_VARIANT%
+call %VSVARS% store
 @IF NOT EXIST "%VCINSTALLDIR%vcvarsphoneall.bat" goto bad_vcvarsphoneall
-@call "%VCINSTALLDIR%vcvarsphoneall.bat" x86_arm
+call "%VCINSTALLDIR%vcvarsphoneall.bat" x86_arm
+@set LIB=%VCINSTALLDIR%lib\store\arm;%LIB%
+@set LIBPATH=%VCINSTALLDIR%lib\store\arm;%LIB%
 @goto run_bash
 
 :setup_WindowsRT
 call %VSVARS%
-call "%VSINSTALLDIR%VC\vcvarsall.bat" x86_arm
 @IF NOT EXIST "%VCINSTALLDIR%vcvarsphoneall.bat" goto bad_vcvarsphoneall
-@call "%VCINSTALLDIR%vcvarsphoneall.bat" x86_arm
+call "%VCINSTALLDIR%vcvarsphoneall.bat" x86_arm
+@set LIB=%VCINSTALLDIR%lib\store\arm;%LIB%
+@set LIBPATH=%VCINSTALLDIR%lib\store\arm;%LIB%
 @goto run_bash
 
 :setup_Windows
@@ -41,13 +41,11 @@ call "%VSINSTALLDIR%VC\vcvarsall.bat" x86
 
 :run_bash
 %WD%%MSYSCON% --hold always /usr/bin/bash scripts/main.sh %*
-@goto end
+@goto :eof
 
 :bad_vcvarsphoneall
 @echo "%VCINSTALLDIR%bin\vcvarsphoneall.bat" needs to be moved into "%VCINSTALLDIR%vcvarsphoneall.bat"
-@IF NOT EXIST "%VS140COMNTOOLS%vsvars32.bat" goto end
+@IF NOT EXIST "%VS140COMNTOOLS%vsvars32.bat" goto :eof
 @echo "%VCINSTALLDIR%bin\x86_arm\vcvarsphonex86_arm.bat" needs to call "%VS140COMNTOOLS%VCVarsPhoneQueryRegistry.bat" not "%~dp0VCVarsPhoneQueryRegistry.bat"
-@echo "%VS140COMNTOOLS%VCVarsPhoneQueryRegistry.bat" needs to look for v8.1 SDKs, not v8.0 ones
-@goto end
-
-:end
+@echo "%VS140COMNTOOLS%VCVarsPhoneQueryRegistry.bat" needs to look for WindowsPhoneApp\v8.1 SDKs, not WindowsPhone\v8.0 ones
+@goto :eof
