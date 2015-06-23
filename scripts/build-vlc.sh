a74a30f898847e275344f93c530bd7552c6749f8
Steve Lhomme <robux4@videolabs.io>
2015-06-23 15:42:20 +0200

    build-vlc: enable directx to get the direct3d11 vout and va
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- scripts/build-vlc.sh
+++ scripts/build-vlc.sh
@@ -93,7 +93,6 @@ if [ ! -f $BUILD_FOLDER/Makefile ] || [ $BUILD_FOLDER/Makefile -ot $SRC_FOLDER/c
         --disable-atmo \
         --disable-vlc \
         --disable-vcd \
-        --disable-directx \
         --disable-smbclient || exit 1
     # Force libtool to generate what we want
     sed -i 's/libname_spec=\"\\$name\"/libname_spec=\"lib\\$name\"/' libtool
