de4e3c8e2a3e1965aab0c6a75fc9c1a427bdb593
Steve Lhomme <robux4@gmail.com>
2016-02-04 09:17:47 +0100

    tools: we need to force building cmake

--- scripts/build-tools.sh
+++ scripts/build-tools.sh
@@ -8,4 +8,5 @@ ROOT_FOLDER=$SCRIPTPATH/..
 cd $ROOT_FOLDER/vlc/extras/tools/
 ./bootstrap || exit 1
 
+# we need a recent cmake as the one in msys2 doesn't support VS2015 output
 make .cmake || exit 1
