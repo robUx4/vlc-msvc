3fa205b722cbaf61afc604ef1b720ae365a84995
Steve Lhomme <robux4@gmail.com>
2016-02-10 16:22:05 +0100

    scripts: rework setup-env.bat to set the proper cmake environment with VS15

--- scripts/setup-env.bat
+++ scripts/setup-env.bat
@@ -1,102 +1,69 @@
-@IF EXIST "%VS140COMNTOOLS%vsvars32.bat" goto vs2015
+@REM adjust the SDK depending on the target
+@IF /I "%1" == "WindowsPhone" goto setup_WindowsPhone
+@IF /I "%1" == "Windows"      goto setup_Windows
+@IF /I "%1" == "Metrox86"     goto setup_Metrox86
+@echo Unknown target "%1"
+@exit -1
+
+
+:setup_WindowsPhone
+@set SDK_VER=8.1
+@set CMAKE_SYSTEM_PROCESSOR=ARM
+@set CMAKE_VS_PLATFORM_TOOLSET=vs120_wp81
+@rem set CMAKE_SYSTEM_NAME=WindowsPhone
+@goto select_vs
+
+:setup_Windows
+@set SDK_VER=8.1
+@set CMAKE_SYSTEM_PROCESSOR=ARM
+@set CMAKE_SYSTEM_NAME=WindowsStore
+@goto select_vs
+
+:setup_Metrox86
+@set SDK_VER=8.1
+@set CMAKE_SYSTEM_PROCESSOR=x86
+@set CMAKE_SYSTEM_NAME=WindowsStore
+@goto select_vs
+
+
+:select_vs
+@REM automatically select the best available compiler
 @IF EXIST "%VS120COMNTOOLS%vsvars32.bat" goto vs2013
+@IF EXIST "%VS140COMNTOOLS%vsvars32.bat" goto vs2015
 @echo VS 2015 or VS 2013 not found
 @exit -1
 
+
 :vs2015
 @set VSVARS="%VS140COMNTOOLS%vsvars32.bat"
-@set STORE_VARIANT=store 8.1
+@set SDK_VARIANT=store %SDK_VER%
 @set CMAKE_VS=Visual Studio 14 2015
 @set VS_RUNTIME=dynamic
 @set VS_TARGET_ARM=amd64_arm
-@set VS_TARGET_X86=amd64_x86
-@set VS_TOOLSET=vs140
+@set VS_TARGET_x86=amd64_x86
+@IF "%CMAKE_VS_PLATFORM_TOOLSET%"=="" set CMAKE_VS_PLATFORM_TOOLSET=vs140
 @goto setupenv
 
 :vs2013
 @set VSVARS="%VS120COMNTOOLS%vsvars32.bat"
-@set STORE_VARIANT=
+@set SDK_VARIANT=
 @set CMAKE_VS=Visual Studio 12 2013
 @set VS_RUNTIME=dynamic
 @set VS_TARGET_ARM=x86_arm
-@set VS_TARGET_X86=x86
-@set VS_TOOLSET=vs120
+@set VS_TARGET_x86=x86
+@IF "%CMAKE_VS_PLATFORM_TOOLSET%"=="" set CMAKE_VS_PLATFORM_TOOLSET=vs120
 @goto setupenv
 
+
 :setupenv
-call %VSVARS% %STORE_VARIANT%
-@IF /I "%1" == "WindowsPhone" goto setup_WindowsPhone
-@IF /I "%1" == "Windows"      goto setup_Windows
-@IF /I "%1" == "Metrox86"     goto setup_Metrox86
+call %VSVARS% %SDK_VARIANT%
+@IF "%CMAKE_SYSTEM_PROCESSOR%"=="ARM" call "%VSINSTALLDIR%VC\vcvarsall.bat" %VS_TARGET_ARM%
+@IF "%CMAKE_SYSTEM_PROCESSOR%"=="ARM" echo call "%VSINSTALLDIR%VC\vcvarsall.bat" %VS_TARGET_ARM%
 
+@IF "%CMAKE_SYSTEM_PROCESSOR%"=="x86" call "%VSINSTALLDIR%VC\vcvarsall.bat" %VS_TARGET_x86%
+@IF "%CMAKE_SYSTEM_PROCESSOR%"=="x86" echo call "%VSINSTALLDIR%VC\vcvarsall.bat" %VS_TARGET_x86%
 
-@REM -----------------------------------------------------------------------
-:GetWindowsPhoneKitDir
-@set WindowsPhoneKitDir=
-@call :GetWindowsPhoneKitDirHelper32 HKLM > nul 2>&1
-@if errorlevel 1 call :GetWindowsPhoneKitDirHelper32 HKCU > nul 2>&1
-@if errorlevel 1 call :GetWindowsPhoneKitDirHelper64 HKLM > nul 2>&1
-@if errorlevel 1 call :GetWindowsPhoneKitDirHelper64 HKCU > nul 2>&1
-@exit /B 0
-
-:GetWindowsPhoneKitDirHelper32
-@for /F "tokens=1,2*" %%i in ('reg query "%1\SOFTWARE\Microsoft\Microsoft SDKs\WindowsPhoneApp\v8.1" /v "InstallationFolder"') DO (
-	@if "%%i"=="InstallationFolder" (
-		@SET WindowsPhoneKitDir=%%k
-	)
-)
-@if "%WindowsPhoneKitDir%"=="" exit /B 1
-@exit /B 0
-
-:GetWindowsPhoneKitDirHelper64
-@for /F "tokens=1,2*" %%i in ('reg query "%1\SOFTWARE\Wow6432Node\Microsoft\Microsoft SDKs\WindowsPhoneApp\v8.1" /v "InstallationFolder"') DO (
-	@if "%%i"=="InstallationFolder" (
-		@SET WindowsPhoneKitDir=%%k
-	)
-)
-@if "%WindowsPhoneKitDir%"=="" exit /B 1
-@exit /B 0
-
-:setup_WindowsPhone
-call "%VSINSTALLDIR%VC\vcvarsall.bat" %VS_TARGET_ARM%
-@rem we may use amd64_arm with VS15 for better speed ?
-@rem call :GetWindowsPhoneKitDir
-@rem set INCLUDE=%WindowsPhoneKitDir%include;%INCLUDE%
-@rem set LIB=%VCINSTALLDIR%lib\store\arm;%WindowsPhoneKitDir%lib\arm;%LIB%
-@rem set LIBPATH=%VCINSTALLDIR%lib\store\arm;%WindowsPhoneKitDir%lib\arm;%LIBPATH%
-@set CMAKE_TARGET=-G "%CMAKE_VS% ARM" -DCMAKE_SYSTEM_NAME=WindowsPhone -DCMAKE_SYSTEM_VERSION=8.1 -DCMAKE_VS_PLATFORM_TOOLSET=%VS_TOOLSET%_wp81
-@rem bogus VS 2015 RC   IF NOT EXIST "%VCINSTALLDIR%vcvarsphoneall.bat" goto bad_vcvarsphoneall
-@rem bogus VS 2015 RC   call "%VCINSTALLDIR%vcvarsphoneall.bat" x86_arm
-@goto run_bash
-
-:setup_Windows
-call "%VSINSTALLDIR%VC\vcvarsall.bat" %VS_TARGET_ARM%
-@rem we may use amd64_arm with VS15 for better speed ?
-@rem set LIB=%VCINSTALLDIR%lib\store\arm;%LIB%
-@rem set LIBPATH=%VCINSTALLDIR%lib\store\arm;%LIBPATH%
-@set CMAKE_TARGET=-G "%CMAKE_VS% ARM" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=8.1 -DCMAKE_VS_PLATFORM_TOOLSET=%VS_TOOLSET%
-@rem bogus VS 2015 RC   IF NOT EXIST "%VCINSTALLDIR%vcvarsphoneall.bat" goto bad_vcvarsphoneall
-@rem bogus VS 2015 RC   call "%VCINSTALLDIR%vcvarsphoneall.bat" x86_arm
-@goto run_bash
-
-:setup_Metrox86
-call "%VSINSTALLDIR%VC\vcvarsall.bat" %VS_TARGET_X86%
-@rem we may use amd64_x86 with VS15 for better speed ?
-@rem set LIB=%VCINSTALLDIR%lib\store;%LIB%
-@rem set LIBPATH=%VCINSTALLDIR%lib\store;%LIBPATH%
-@set CMAKE_TARGET=-G "%CMAKE_VS%" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=8.1 -DCMAKE_VS_PLATFORM_TOOLSET=%VS_TOOLSET%
-@rem bogus VS 2015 RC   IF NOT EXIST "%VCINSTALLDIR%vcvarsphoneall.bat" goto bad_vcvarsphoneall
-@rem bogus VS 2015 RC   call "%VCINSTALLDIR%vcvarsphoneall.bat" x86
-@goto run_bash
-
-
-:run_bash
+@IF "%CMAKE_SYSTEM_PROCESSOR%"=="ARM" set CMAKE_VS=%CMAKE_VS% ARM
+@set CMAKE_SYSTEM_VERSION=%SDK_VER%
+@set CMAKE_TARGET=-G "%CMAKE_VS%" -DCMAKE_SYSTEM_VERSION=%CMAKE_SYSTEM_VERSION% -DCMAKE_VS_PLATFORM_TOOLSET=%CMAKE_VS_PLATFORM_TOOLSET% -DCMAKE_SYSTEM_PROCESSOR=%CMAKE_SYSTEM_PROCESSOR%
 %WD%%MSYSCON% --hold always /usr/bin/bash scripts/main.sh %*
-@goto :eof
-
-:bad_vcvarsphoneall
-@echo "%VCINSTALLDIR%bin\vcvarsphoneall.bat" needs to be moved into "%VCINSTALLDIR%vcvarsphoneall.bat"
-@IF NOT EXIST "%VS140COMNTOOLS%vsvars32.bat" goto :eof
-@echo "%VCINSTALLDIR%bin\x86_arm\vcvarsphonex86_arm.bat" needs to call "%VS140COMNTOOLS%VCVarsPhoneQueryRegistry.bat" not "%~dp0VCVarsPhoneQueryRegistry.bat"
-@echo "%VS140COMNTOOLS%VCVarsPhoneQueryRegistry.bat" needs to look for WindowsPhoneApp\v8.1 SDKs, not WindowsPhone\v8.0 ones
-@goto :eof
