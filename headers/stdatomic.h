ccab0c66a2174ed40a5b31ee61e64219869c1dc3
Steve Lhomme <robux4@videolabs.io>
2016-06-03 11:13:01 +0200

    atomics on windows must be 32 bits at least

--- headers/stdatomic.h
+++ headers/stdatomic.h
@@ -1,6 +1,8 @@
-#ifndef __cplusplus
+#if !defined(__cplusplus) && defined(_MSC_VER)
 
-# include <windows.h>
+#include <windows.h>
+
+#define  memory_order_relaxed 0
 
 /* Define macros in order to dispatch to the correct function depending on the type.
    Several ranges are need because some operations are not implemented for all types. */
@@ -9,27 +11,21 @@
     sizeof(*object) == 8 ? operation##64((LONGLONG *)object, __VA_ARGS__) : \
     (abort(), 0))
 
-#  define atomic_type_dispatch_16_64(operation, object, ...) \
-    (sizeof(*object) == 2 ? operation##16((short *)object, __VA_ARGS__) : \
-    atomic_type_dispatch_32_64(operation, object, __VA_ARGS__))
-
-#  define atomic_type_dispatch_8_64(operation, object, ...) \
-    (sizeof(*object) == 1 ? operation##8((char *)object, __VA_ARGS__) : \
-    atomic_type_dispatch_16_64(operation, object, __VA_ARGS__))
-
 #  define atomic_exchange(object,desired) \
-    atomic_type_dispatch_16_64(InterlockedExchange, object, desired)
+    atomic_type_dispatch_32_64(InterlockedExchange, object, desired)
 
 #  define atomic_load(object) \
-    atomic_type_dispatch_16_64(InterlockedCompareExchange, object, 0, 0)
+    atomic_type_dispatch_32_64(InterlockedCompareExchange, object, 0, 0)
+#  define atomic_load_explicit(object,order) \
+    atomic_load(object)
 
 #  define atomic_store(object,desired) \
-    atomic_type_dispatch_16_64(InterlockedExchange, object, desired)
+    atomic_type_dispatch_32_64(InterlockedExchange, object, desired)
 #  define atomic_store_explicit(object,desired,order) \
     atomic_store(object, desired)
 
 #  define atomic_compare_exchange_strong(object,expected,desired) \
-    atomic_type_dispatch_16_64(InterlockedCompareExchange, object, *expected, desired) == *expected
+    atomic_type_dispatch_32_64(InterlockedCompareExchange, object, *expected, desired) == *expected
 #  define atomic_compare_exchange_weak(object,expected,desired) \
     atomic_compare_exchange_strong(object, expected, desired)
 
@@ -40,21 +36,29 @@
     atomic_type_dispatch_32_64(InterlockedExchangeAdd, object, -(LONGLONG)operand)
 
 #  define atomic_fetch_or(object,operand) \
-    atomic_type_dispatch_8_64(InterlockedOr, object, operand)
+    atomic_type_dispatch_32_64(InterlockedOr, object, operand)
 #  define atomic_fetch_or_explicit(object,operand,order) \
     atomic_fetch_or(object, operand)
 
 #  define atomic_fetch_and(object,operand) \
-    atomic_type_dispatch_8_64(InterlockedAnd, object, operand)
+    atomic_type_dispatch_32_64(InterlockedAnd, object, operand)
 #  define atomic_fetch_and_explicit(object,operand,order) \
     atomic_fetch_and(object, operand)
 
 /* atomic types */
+/* Some atomic operations of the Interlocked API are only
+   available for desktop apps. Thus we define the atomic types to
+   be at least 32 bits wide. */
+typedef      int_least32_t atomic_flag;
+typedef      int_least32_t atomic_bool;
+typedef      int_least32_t atomic_char;
+typedef      int_least32_t atomic_schar;
+typedef     uint_least32_t atomic_uchar;
+typedef      int_least32_t atomic_short;
+typedef     uint_least32_t atomic_ushort;
+
 typedef          int       atomic_int;
 typedef unsigned int       atomic_uint;
-typedef          bool      atomic_bool;
-typedef unsigned char      atomic_uchar;
-typedef unsigned short     atomic_ushort;
 typedef         uintptr_t atomic_uintptr_t;
 
 typedef     uint_fast32_t atomic_uint_fast32_t;
