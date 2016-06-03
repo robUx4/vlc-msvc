ae1d9d182539e2361bdc2e5df9dbfde1ed76a66a
Steve Lhomme <robux4@videolabs.io>
2016-06-03 11:13:41 +0200

    fixup: bring back the _() define

--- headers/fixup.h
+++ headers/fixup.h
@@ -30,7 +30,7 @@ typedef signed long int ssize_t;
 typedef signed long int off_t;
 typedef unsigned int mode_t;
 //# define N_(x) x
-//# define _(x) x
+# define _(x) x
 
 # define alloca _alloca
 # define bzero(a,b) memset( (a), 0, (b) )
