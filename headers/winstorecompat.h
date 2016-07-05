a0d0f98792d6d0fdfe1314e08989da9be9b8905f
Steve Lhomme <robux4@videolabs.io>
2016-07-05 17:14:07 +0200

    winstorecompat: workaround for an SDK incompatibility when building AMD64

--- headers/winstorecompat.h
+++ headers/winstorecompat.h
@@ -10,6 +10,10 @@
 #pragma warning(pop)
 #endif /* _MSV_VER */
 
+#if defined(__clang__)
+#define __PRFCHWINTRIN_H /* workaround for an SDK incompatibility when building AMD64 */
+#endif /*__clang__ */
+
 /* not detected properly by autoconf due to early forced include */
 #define HAVE_WCSCPY 1
 #define HAVE_WCSLEN 1
