91ec00697ab41ba3656469ff9df9576a53c88843
Steve Lhomme <robux4@videolabs.io>
2016-07-08 18:30:02 +0200

    wincrypt: add more entries and defines for gnutls

--- headers/wincrypt.h
+++ headers/wincrypt.h
@@ -32,6 +32,7 @@ typedef __x_ABI_CWindows_CSecurity_CCryptography_CCertificates_CICertificateStor
 DEFINE_GUID(IID_ICertificateStoresStatics,   0xFBECC739, 0xC6FE, 0x4DE7, 0x99, 0xCF, 0x74, 0xC3, 0xE5, 0x96, 0xE0, 0x32);
 # endif /* _MSC_VER */
 
+#define HCRYPTPROV_DEFINED
 typedef void *HCRYPTPROV_LEGACY;
 typedef ICertificateStore *HCERTSTORE;
 
@@ -47,6 +48,37 @@ typedef struct {
     DWORD cbCrlEncoded;
 } CRL_CONTEXT;
 typedef const CRL_CONTEXT *PCCRL_CONTEXT;
+typedef struct _CRYPTOAPI_BLOB {
+    DWORD cbData;
+    BYTE  *pbData;
+} CRYPT_INTEGER_BLOB, *PCRYPT_INTEGER_BLOB,
+CRYPT_UINT_BLOB, *PCRYPT_UINT_BLOB,
+CRYPT_OBJID_BLOB, *PCRYPT_OBJID_BLOB,
+CERT_NAME_BLOB, *PCERT_NAME_BLOB,
+CERT_RDN_VALUE_BLOB, *PCERT_RDN_VALUE_BLOB,
+CERT_BLOB, *PCERT_BLOB,
+CRL_BLOB, *PCRL_BLOB,
+DATA_BLOB, *PDATA_BLOB,
+CRYPT_DATA_BLOB, *PCRYPT_DATA_BLOB,
+CRYPT_HASH_BLOB, *PCRYPT_HASH_BLOB,
+CRYPT_DIGEST_BLOB, *PCRYPT_DIGEST_BLOB,
+CRYPT_DER_BLOB, *PCRYPT_DER_BLOB,
+CRYPT_ATTR_BLOB, *PCRYPT_ATTR_BLOB;
+typedef struct _CRYPT_KEY_PROV_PARAM {
+    DWORD           dwParam;
+    BYTE            *pbData;
+    DWORD           cbData;
+    DWORD           dwFlags;
+} CRYPT_KEY_PROV_PARAM, *PCRYPT_KEY_PROV_PARAM;
+typedef struct _CRYPT_KEY_PROV_INFO {
+    LPWSTR                  pwszContainerName;
+    LPWSTR                  pwszProvName;
+    DWORD                   dwProvType;
+    DWORD                   dwFlags;
+    DWORD                   cProvParam;
+    PCRYPT_KEY_PROV_PARAM   rgProvParam;
+    DWORD                   dwKeySpec;
+} CRYPT_KEY_PROV_INFO, *PCRYPT_KEY_PROV_INFO;
 
 #define X509_ASN_ENCODING  1
 
@@ -55,6 +87,69 @@ typedef const CRL_CONTEXT *PCCRL_CONTEXT;
 #define MS_DEF_PROV          L"Microsoft Base Cryptographic Provider v1.0"
 #define CRYPT_SILENT         0x40
 #define CRYPT_VERIFYCONTEXT  0xF0000000
+
+#define CERT_KEY_PROV_HANDLE_PROP_ID        1
+#define CERT_KEY_PROV_INFO_PROP_ID          2
+#define CERT_SHA1_HASH_PROP_ID              3
+#define CERT_MD5_HASH_PROP_ID               4
+#define CERT_HASH_PROP_ID                   CERT_SHA1_HASH_PROP_ID
+#define CERT_KEY_CONTEXT_PROP_ID            5
+#define CERT_KEY_SPEC_PROP_ID               6
+#define CERT_IE30_RESERVED_PROP_ID          7
+#define CERT_PUBKEY_HASH_RESERVED_PROP_ID   8
+#define CERT_ENHKEY_USAGE_PROP_ID           9
+#define CERT_CTL_USAGE_PROP_ID              CERT_ENHKEY_USAGE_PROP_ID
+#define CERT_NEXT_UPDATE_LOCATION_PROP_ID   10
+#define CERT_FRIENDLY_NAME_PROP_ID          11
+#define CERT_PVK_FILE_PROP_ID               12
+#define CERT_DESCRIPTION_PROP_ID            13
+#define CERT_ACCESS_STATE_PROP_ID           14
+#define CERT_SIGNATURE_HASH_PROP_ID         15
+#define CERT_SMART_CARD_DATA_PROP_ID        16
+#define CERT_EFS_PROP_ID                    17
+#define CERT_FORTEZZA_DATA_PROP_ID          18
+#define CERT_ARCHIVED_PROP_ID               19
+#define CERT_KEY_IDENTIFIER_PROP_ID         20
+#define CERT_AUTO_ENROLL_PROP_ID            21
+#define CERT_PUBKEY_ALG_PARA_PROP_ID        22
+#define CERT_CROSS_CERT_DIST_POINTS_PROP_ID 23
+#define CERT_ISSUER_PUBLIC_KEY_MD5_HASH_PROP_ID     24
+#define CERT_SUBJECT_PUBLIC_KEY_MD5_HASH_PROP_ID    25
+#define CERT_ENROLLMENT_PROP_ID             26
+#define CERT_DATE_STAMP_PROP_ID             27
+#define CERT_ISSUER_SERIAL_NUMBER_MD5_HASH_PROP_ID  28
+#define CERT_SUBJECT_NAME_MD5_HASH_PROP_ID  29
+#define CERT_EXTENDED_ERROR_INFO_PROP_ID    30
+
+#define CERT_COMPARE_MASK           0xFFFF
+#define CERT_COMPARE_SHIFT          16
+#define CERT_COMPARE_ANY            0
+#define CERT_COMPARE_SHA1_HASH      1
+#define CERT_COMPARE_NAME           2
+#define CERT_COMPARE_ATTR           3
+#define CERT_COMPARE_MD5_HASH       4
+#define CERT_COMPARE_PROPERTY       5
+#define CERT_COMPARE_PUBLIC_KEY     6
+#define CERT_COMPARE_HASH           CERT_COMPARE_SHA1_HASH
+#define CERT_COMPARE_NAME_STR_A     7
+#define CERT_COMPARE_NAME_STR_W     8
+#define CERT_COMPARE_KEY_SPEC       9
+#define CERT_COMPARE_ENHKEY_USAGE   10
+#define CERT_COMPARE_CTL_USAGE      CERT_COMPARE_ENHKEY_USAGE
+#define CERT_COMPARE_SUBJECT_CERT   11
+#define CERT_COMPARE_ISSUER_OF      12
+#define CERT_COMPARE_EXISTING       13
+#define CERT_COMPARE_SIGNATURE_HASH 14
+#define CERT_COMPARE_KEY_IDENTIFIER 15
+#define CERT_COMPARE_CERT_ID        16
+#define CERT_COMPARE_CROSS_CERT_DIST_POINTS 17
+
+#define CERT_FIND_ANY           (CERT_COMPARE_ANY << CERT_COMPARE_SHIFT)
+#define CERT_FIND_SHA1_HASH     (CERT_COMPARE_SHA1_HASH << CERT_COMPARE_SHIFT)
+#define CERT_FIND_MD5_HASH      (CERT_COMPARE_MD5_HASH << CERT_COMPARE_SHIFT)
+#define CERT_FIND_SIGNATURE_HASH (CERT_COMPARE_SIGNATURE_HASH << CERT_COMPARE_SHIFT)
+#define CERT_FIND_KEY_IDENTIFIER (CERT_COMPARE_KEY_IDENTIFIER << CERT_COMPARE_SHIFT)
+#define CERT_FIND_HASH          CERT_FIND_SHA1_HASH
 
 typedef ICryptographicBufferStatics *HCRYPTPROV;
 
@@ -176,11 +271,32 @@ __forceinline PCCRL_CONTEXT CertEnumCRLsInStore(HCERTSTORE hCertStore, PCCRL_CON
 }
 
 #define Loaded_CertEnumCRLsInStore CertEnumCRLsInStore
+
+__forceinline BOOL CertGetCertificateContextProperty(PCCERT_CONTEXT pCertContext, DWORD dwPropId, void *pvData, DWORD *pcbData)
+{
+    return FALSE;
+}
+
+__forceinline BOOL CertDeleteCertificateFromStore(PCCERT_CONTEXT pCertContext)
+{
+    return FALSE;
+}
+
+__forceinline HCERTSTORE PFXImportCertStore(CRYPT_DATA_BLOB* pPFX, LPCWSTR szPassword, DWORD dwFlags)
+{
+    return NULL;
+}
 #else
 WINCRYPT32API HCERTSTORE WINAPI CertOpenStore(LPCSTR lpszStoreProvider, DWORD dwEncodingType, HCRYPTPROV_LEGACY hCryptProv, DWORD dwFlags, const void *pvPara);
+WINCRYPT32API HCERTSTORE WINAPI CertOpenSystemStoreW(HCRYPTPROV_LEGACY hProv, LPCWSTR szSubsystemProtocol);
+#define CertOpenSystemStore  CertOpenSystemStoreW
 WINCRYPT32API BOOL WINAPI CertCloseStore(HCERTSTORE hCertStore, DWORD dwFlags);
 WINCRYPT32API PCCRL_CONTEXT WINAPI CertEnumCRLsInStore(HCERTSTORE hCertStore, PCCRL_CONTEXT pPrevCrlContext);
 WINCRYPT32API PCCERT_CONTEXT WINAPI CertEnumCertificatesInStore(HCERTSTORE hCertStore, PCCERT_CONTEXT pPrevCertContext);
+WINCRYPT32API PCCERT_CONTEXT WINAPI CertFindCertificateInStore(HCERTSTORE hCertStore, DWORD dwCertEncodingType, DWORD dwFindFlags, DWORD dwFindType, const void *pvFindPara, PCCERT_CONTEXT pPrevCertContext);
+WINCRYPT32API BOOL WINAPI CertGetCertificateContextProperty(PCCERT_CONTEXT pCertContext, DWORD dwPropId, void *pvData, DWORD *pcbData);
+WINCRYPT32API BOOL WINAPI CertDeleteCertificateFromStore(PCCERT_CONTEXT pCertContext);
+WINCRYPT32API HCERTSTORE WINAPI PFXImportCertStore(CRYPT_DATA_BLOB* pPFX, LPCWSTR szPassword, DWORD dwFlags);
 #endif /* _WIN32_WINNT */
 
 // By default, when the CurrentUser "Root" store is opened, any SystemRegistry
