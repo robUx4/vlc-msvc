af731b46c6f918ee7620d31da40e9310298a21fb
Steve Lhomme <robux4@videolabs.io>
2016-10-06 11:05:33 +0200

    fixup: add some missing defined translations

--- headers/fixup.h
+++ headers/fixup.h
@@ -90,6 +90,11 @@ typedef unsigned int mode_t;
 #define O_CREAT   _O_CREAT
 #define O_TRUNC   _O_TRUNC
 #define O_BINARY  _O_BINARY
+#define O_APPEND  _O_APPEND
+#define O_EXCL    _O_EXCL
+#define O_RDWR    _O_RDWR
+#define O_TEXT    _O_TEXT
+#define O_NOINHERIT _O_NOINHERIT
 
 # ifndef S_ISREG
 #  define S_ISREG(m) (((m) & S_IFMT) == S_IFREG)
