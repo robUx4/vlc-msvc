fe91c12d42ecfeaf0bdb67af65d7da36e8132f0e
Steve Lhomme <robux4@videolabs.io>
2016-10-25 12:56:39 +0200

    build-tools: no need to build CMake anymore
    let the normal make do its job

--- scripts/build-tools.sh
+++ scripts/build-tools.sh
@@ -9,4 +9,5 @@ cd $ROOT_FOLDER/vlc/extras/tools/
 ./bootstrap || exit 1
 
 # we need a recent cmake as the one in msys2 doesn't support VS2015 output
-CXXFLAGS="-static -static-libgcc -static-libstdc++" CFLAGS="-static -static-libgcc -static-libstdc++" make .cmake || exit 1
+#CXXFLAGS="-static -static-libgcc -static-libstdc++" CFLAGS="-static -static-libgcc -static-libstdc++" make .cmake || exit 1
+make || exit 1
